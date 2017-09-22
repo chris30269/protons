d3d82ab1e1f7453bd87ced5f8c63628e11a444a7
chris30269 <cernst3@gatech.edu>
2017-09-22 15:21:58 -0400

    Create data.js

---
+++ data.js
@@ -0,0 +1,66 @@
+var food = {
+	1:{
+		"restaurant":1
+		"name":"SMOKED ITALIAN PORCHETTA",
+		"protein":30,
+		"calories":690
+	},
+	2:{
+		"restaurant":1
+		"name":"Mount Italy",
+		"protein":52,
+		"calories":950
+	},
+	3:{
+		"restaurant":1
+		"name":"Loaded Italian",
+		"protein":32,
+		"calories":680
+	},
+	4:{
+		"restaurant":1
+		"name":"Chicken Cordon Bleu",
+		"protein":41,
+		"calories":690
+	},
+	5:{
+		"restaurant":1
+		"name":"Half Pound Roast Beef",
+		"protein":48,
+		"calories":610
+	},
+	6:{
+		"restaurant":1
+		"name":"TRIPLE THICK BROWN SUGAR BACON HALF POUND CLUB",
+		"protein":58,
+		"calories":830
+	},
+	7:{
+		"restaurant":1
+		"name":"TRIPLE THICK BROWN SUGAR BACON TURKEY",
+		"protein":34,
+		"calories":690
+	},
+	8:{
+		"restaurant":1
+		"name":"Pizza Slider",
+		"protein":13,
+		"calories":300
+	},
+	9:{
+		"restaurant":1
+		"name":"turkey cheese slider",
+		"protein":14,
+		"calories":200
+	},
+	10:{
+		"restaurant":1
+		"name":"roast beef n cheese slider",
+		"protein":14,
+		"calories":240
+	}
+};
+
+var restaurants = {
+	1:"Arby's"
+};

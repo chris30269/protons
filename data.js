1066dc0be6ed9ecc26533109ac2b9e77df210c04
Ernst, Chris R <Chris.Ernst@elavon.com>
2017-10-18 11:40:08 -0400

    stuff and things

--- data.js
+++ data.js
@@ -1,83 +1,97 @@
-var food = {
-	1:{
+var food = [
+	{
 		"restaurant":1,
 		"name":"SMOKED ITALIAN PORCHETTA",
 		"protein":30,
-		"calories":690
+		"calories":690,
+		"price":5
 	},
-	2:{
+	{
 		"restaurant":1,
 		"name":"Mount Italy",
 		"protein":52,
-		"calories":950
+		"calories":950,
+		"price":5
 	},
-	3:{
+	{
 		"restaurant":1,
 		"name":"Loaded Italian",
 		"protein":32,
-		"calories":680
+		"calories":680,
+		"price":5
 	},
-	4:{
+	{
 		"restaurant":1,
 		"name":"Chicken Cordon Bleu",
 		"protein":41,
-		"calories":690
+		"calories":690,
+		"price":5
 	},
-	5:{
+	{
 		"restaurant":1,
 		"name":"Half Pound Roast Beef",
 		"protein":48,
-		"calories":610
+		"calories":610,
+		"price":5
 	},
-	6:{
+	{
 		"restaurant":1,
 		"name":"TRIPLE THICK BROWN SUGAR BACON HALF POUND CLUB",
 		"protein":58,
-		"calories":830
+		"calories":830,
+		"price":5
 	},
-	7:{
+	{
 		"restaurant":1,
 		"name":"TRIPLE THICK BROWN SUGAR BACON TURKEY",
 		"protein":34,
-		"calories":690
+		"calories":690,
+		"price":5
 	},
-	8:{
+	{
 		"restaurant":1,
 		"name":"Pizza Slider",
 		"protein":13,
-		"calories":300
+		"calories":300,
+		"price":1.3
 	},
-	9:{
+	{
 		"restaurant":1,
 		"name":"turkey cheese slider",
 		"protein":14,
-		"calories":200
+		"calories":200,
+		"price":1.3
 	},
-	10:{
+	{
 		"restaurant":1,
 		"name":"roast beef n cheese slider",
 		"protein":14,
-		"calories":240
+		"calories":240,
+		"price":1.3
 	},
 	11:{
 		"restaurant":1,
 		"name":"DEEP FRIED TURKEY GOBBLER"
 		"calories":780,
-		"protein":54
+		"protein":54,
+		"price":1.3
 	},
 	12:{
 		"restaurant":1,
 		"name":"CAJUN DEEP FRIED TURKEY"
 		"calories":430,
-		"protein":25
+		"protein":25,
+		"price":1.3
 	},
 	13:{
 		"restaurant":1,
 		"name":"DEEP FRIED TURKEY CLUB"
 		"calories":540,
 		"protein":34
+		"calories":240,
+		"price":1.3
 	}
-};
+];
 
 var restaurants = {
 	1:"Arby's"

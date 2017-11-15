86fc73b943ec4e88fadf96e232a3c5f4fdf63f15
Ernst, Chris R <Chris.Ernst@elavon.com>
2017-11-15 09:26:23 -0500

    more better everything

--- data.js
+++ data.js
@@ -88,11 +88,144 @@ var food = [
 		"name":"DEEP FRIED TURKEY CLUB",
 		"calories":540,
 		"protein":34,
-		"calories":540,
+		"price":1.3
+	},
+	{
+		"restaurant":2,
+		"name":"Dressed Egg Taco - Bacon",
+		"calories":470,
+		"protein":20,
+		"price":1.3
+	},
+	{
+		"restaurant":2,
+		"name":"Dressed Egg Taco - Sausage",
+		"calories":460,
+		"protein":17,
+		"price":1.3
+	},
+	{
+		"restaurant":2,
+		"name":"Naked Egg Taco - Bacon",
+		"calories":310,
+		"protein":14,
+		"price":1.3
+	},
+	{
+		"restaurant":2,
+		"name":"Naked Egg Taco - Sausage",
+		"calories":300,
+		"protein":12,
+		"price":1.3
+	},
+	{
+		"restaurant":2,
+		"name":"XXL Grilled Stuft Burrito - Chicken",
+		"calories":830,
+		"protein":38,
+		"price":1.3
+	},
+	{
+		"restaurant":2,
+		"name":"XXL Grilled Stuft Burrito - Steak",
+		"calories":840,
+		"protein":37,
+		"price":1.3
+	},
+	{
+		"restaurant":2,
+		"name":"Smothered Burrito - Steak",
+		"calories":660,
+		"protein":33,
+		"price":1.3
+	},
+	{
+		"restaurant":2,
+		"name":"Naked Chicken Chips (12 Pieces)",
+		"calories":780,
+		"protein":28,
+		"price":1.3
+	},
+	{
+		"restaurant":2,
+		"name":"Crispy Chicken Quesadilla",
+		"calories":650,
+		"protein":26,
+		"price":1.3
+	},
+	{
+		"restaurant":2,
+		"name":"Power Menu Bowl - Chicken",
+		"calories":500,
+		"protein":28,
+		"price":1.3
+	},
+	{
+		"restaurant":2,
+		"name":"Quesadilla - Chicken",
+		"calories":510,
+		"protein":27,
+		"price":1.3
+	},
+	{
+		"restaurant":2,
+		"name":"Soft Taco - Beef",
+		"calories":180,
+		"protein":9,
+		"price":1.3
+	},
+	{
+		"restaurant":2,
+		"name":"Soft Taco - Chicken",
+		"calories":170,
+		"protein":10,
+		"price":1.3
+	},
+	{
+		"restaurant":2,
+		"name":"Breakfast Quesadilla - Steak",
+		"calories":500,
+		"protein":29,
+		"price":1.3
+	},
+	{
+		"restaurant":2,
+		"name":"Breakfast Crunchwrap - Sausage",
+		"calories":700,
+		"protein":21,
+		"price":1.3
+	},
+	{
+		"restaurant":2,
+		"name":"Grande Scrambler Burrito - Sausage",
+		"calories":640,
+		"protein":19,
+		"price":1.3
+	},
+	{
+		"restaurant":2,
+		"name":"Sausage Flatbread Quesadilla",
+		"calories":330,
+		"protein":14,
+		"price":1.3
+	},
+	{
+		"restaurant":2,
+		"name":"Beefy 5-Layer Burrito",
+		"calories":500,
+		"protein":19,
 		"price":1.3
 	}
 ];
 
 var restaurants = {
-	1:"Arby's"
+	1:"Arby's",
+	2:"Taco Bell",
+	3:"Grocery"
 };
+ var colors = {
+ 	0:"coral",
+ 	1:"#d71921",
+ 	2:"#8328b2",
+ 	3:"lightgrey"
+ };
